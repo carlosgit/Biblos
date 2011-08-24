bb1ccaf074fa5dc653d79aac2b952102f992d70a
Jose Vallecillo <joseantonio_vallecillo1@hotmail.com>
2011-08-24 13:35:56 +0200

    Modificaciones sobre el menu general

--- menuG.php
+++ menuG.php
@@ -11,20 +11,47 @@ controlSesion();
     </head>
     <body>
         <h1>MENU</h1>
-        <ul>
-            <ul><li>Consultas
-                    <ul>
-                        <li><a href='cgeneral.php'>Consulta general</a>
-                        <li><a href='cespecificaG.php'>Consulta especifica</a>
-                    </ul> </ul>                   
-            <ul><li>Administración
-                    <ul>
-                        <li><a href='gestionlibrog.php'>Gestión Catalogo</a>
-                        <li><a href='gestionusuariog.php'>Gestion Usuario</a>
-                    </ul>            
+        <?php
+        if (!$_SESSION['usuario'] || !isset($_SESSION['usuario'])) {
+            echo "logeate primero";
+        } else {
+            $usuario = $_SESSION['usuario'];
+            echo "Usuario:" . $usuario['nombre_usuario'];
+        }
+        ?>
+        <h1>Opciones</h1>
+        <ul><li>Consultas
+                <ul>
+                    <li><a href='cgeneral.php'>Consulta general</a>
+                    <li><a href='cespecificaG.php'>Consulta concreta</a>
 
-            </ul>
-            <li><a href='salir.php'>Log out</a>
+                </ul>
+
+
+                <?php
+                // Comprobacion del tipo de usuario
+                if ($usuario['es_administrador'] == 1)
+                    mostrarOpcionesAdministracion();
+                ?>
+
+            <li><a href='salir.php'>Salir</a>
         </ul>
-    </body>
+    </ul>
+</body>
 </html>
+
+
+<?php
+
+function mostrarOpcionesAdministracion() {
+//echo "Es administrador";
+
+    echo("
+        <li>Administracion
+        <ul>
+        <li><a href='gestionlibrog.php'>Gestion Catalogo</a>      
+        <li><a href='gestionusuariog.php'>Gestion Usuario</a>        
+        </ul>");
+}
+
+?>
